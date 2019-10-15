d45ac0ce98b39417bdeb92cf07764d5f163e4cc0
anushgandra <anushgandra@yahoo.com>
2019-10-15 11:35:21 -0700

    10/15/19

--- nn/layers/prelu_layer.py
+++ nn/layers/prelu_layer.py
@@ -10,19 +10,14 @@ class PReLULayer(Layer):
         super(PReLULayer, self).__init__(parent)
         self.slope = Parameter(np.full(size, initial_slope))
         self.data = None
+        self.size = size
 
     def forward(self, data):
-        out1 = np.copy(data)
-        out2 = np.copy(data)
-
-        out1 = np.moveaxis(out1,1,2)
-
-        out1[out1>0] = 0
-        out1 = out1*self.slope.data
-
-        out1 = np.moveaxis(out1,2,1)
-
-        out2[out2<=0] = 0
+        
+        out1 = np.minimum(0,data)
+        out1 = out1*(np.reshape(self.slope.data,(1,self.size,1)))
+                     
+        out2 = np.maximum(0,data)
         out = out1+out2
 
         self.data = data
@@ -30,19 +25,15 @@ class PReLULayer(Layer):
     
 
     def backward(self, previous_partial_gradient):
-        out3 = np.copy(self.data)
-        out3[out3>0] = 1
-        out3[out3<=0] = 0
+        out = np.copy(self.data)
+        temp1 = np.argwhere(out>0)
+        out[out<=0] = 1
+        
+        out = out*np.reshape(self.slope.data,(1,self.size,1))
+        
+        out[temp1[:,0],temp1[:,1]] = 1
 
-        out4 = np.copy(self.data)
-        temp1 = np.argwhere(out4>0)
-        out4[out4<=0] = 1
-        out4 = np.moveaxis(out4,1,2)
-        out4 = out4*self.slope.data
-        out4 = np.moveaxis(out4,2,1)
-        out4[temp1[:,0],temp1[:,1]] = 0
-
-        out = out3+out4
+        output = np.multiply(out,previous_partial_gradient)
 
         grad_mult = np.copy(self.data)
         grad_mult[grad_mult>0] = 0
@@ -50,4 +41,4 @@ class PReLULayer(Layer):
         grad = np.multiply(previous_partial_gradient,grad_mult)
 
         self.slope.grad = np.sum(np.sum(grad,axis=0),axis=1)
-        return out
+        return output
