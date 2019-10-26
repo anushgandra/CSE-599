f062d7d58f11aa0090a4d6237350f264c30ef521
anushgandra <anushgandra@yahoo.com>
2019-10-26 15:48:12 -0700

    10/26/19

--- nn/layers/conv_layer.py
+++ nn/layers/conv_layer.py
@@ -21,89 +21,104 @@ class ConvLayer(Layer):
 
     @staticmethod
     @njit(parallel=True, cache=True)
-    def forward_numba(data, weights, bias, stride, padding, kernel_size,input_shape,weight_shape,out):
+    def forward_numba(data, weights, bias, stride, kernel_size,out):
         # TODO
         # data is N x C x H x W
         # kernel is COld x CNew x K x K
         
-        s = input_shape
+        s = data.shape
         n = s[0]
         h = s[2]
         w = s[3]
 
-        ws = weight_shape
+        ws = weights.shape
         c = ws[1]
 
-        jlim = (h-kernel_size) + 1 + padding
-        ilim = (w-kernel_size) + 1 + padding
+        jlim = (h-kernel_size) + 1 #+ padding
+        ilim = (w-kernel_size) + 1 #+ padding
+
+##        print("prev_shape: ",prev_shape)
+##        print("padded data shape: ",s)
+##        print("jlim: ",jlim)
+##        print("ilim: ",ilim)
+##        print("Weight shape: ",weights.shape)
+##        print("padding: ",padding)
              
         for l in prange(n):
             for o in prange(c):
-                for j in range(padding,jlim,stride):
-                    for i in range(padding,ilim,stride):
-                        out[l,o,(j-padding)//stride,(i-padding)//stride] = np.sum((data[l,:,j-(padding):j+(kernel_size)-padding,i-(padding):i+(kernel_size)-padding]*weights[:,o,:,:]))+bias[o]
+                for j in range(0,jlim,stride):
+                    for i in range(0,ilim,stride):
+                        out[l,o,j//stride,i//stride] = np.sum((data[l,:,j:j+(kernel_size),i:i+(kernel_size)]*weights[:,o,:,:]))+bias[o]
                                                                         
         return out
 
     def forward(self, data):
         padded = np.pad(data,((0,0),(0,0),(self.padding,self.padding),(self.padding,self.padding)),'constant')
         output_array = np.zeros((data.shape[0],self.weight.data.shape[1],(padded.shape[2]-self.kernel_size)//self.stride + 1,(padded.shape[3]-self.kernel_size)//self.stride + 1),dtype=np.float32)
-        output = self.forward_numba(padded, self.weight.data, self.bias.data, self.stride, self.padding, self.kernel_size,padded.shape,self.weight.data.shape,output_array)
+
+        output = self.forward_numba(padded, self.weight.data, self.bias.data, self.stride, self.kernel_size,output_array)
+        
         self.data = data
         self.padded_data = padded
         return output
 
     @staticmethod
     @njit(cache=True, parallel=True)
-    def backward_numba(padded_data, padded_data_transpose, output, prev_grad, padded_prev_grad, weights, weights_transpose, weights_grad, padding, stride, kernel_size, padded_data_shape, prev_shape, padding2):
+    def backward_numba(padded_data, prev_grad, weight, weight_grad, bias_grad,output, padded_output, padding, stride):
         # TODO
         # data is N x C x H x W
         # kernel is COld x CNew x K x K
+        [n,d,hin,win] =  padded_data.shape
+        [n,c,hout,wout] = prev_grad.shape
+        
+        k = weight.shape[2]
         # First I'll compute weights_grad
         
-        s = padded_data_shape
-        n = s[0]
-        d = s[1]
-        h = s[2]
-        w = s[3]
+##        jlim = (hin-hout) + 1
+##        ilim = (win-wout) + 1
 
-        c = prev_shape[1]
+##        for l in range(d):
+##            for o in range(c):
+##                for j in range(0,jlim,stride):
+##                    for i in range(0,ilim,stride):
+##                        weight_grad[l,o,j//stride,i//stride] = np.sum(padded_data_transpose[l,:,j:j+k,i:i+k]*prev_grad[:,c,:,:])
+                        
+        # Now for gradient with respect to inputs and bias grad
 
-        jlim = (h-prev_shape[2]) + 1 + padding
-        ilim = (w-prev_shape[3]) + 1 + padding
-             
-        for l in prange(d):
-            for o in prange(c):
-                for k in range(n):
-                    for j in range(padding,jlim,stride):
-                        for i in range(padding,ilim,stride):
-                            for n in range(prev_shape[2]):
-                                for m in range(prev_shape[3]):
-                                    weights_grad[l,o,(j-padding)//stride,(i-padding)//stride] += (padded_data_transpose[l,k,j-(padding)+n,i-(padding)+m])*prev_grad[k,o,n,m]
+##        jlim = hout
+##        ilim = wout
 
-        jlim = ((prev_shape[2]+(2*padding2)) - kernel_size) + 1 + padding2
-        ilim = ((prev_shape[3]+(2*padding2)) - kernel_size) + 1 + padding2
-        
-        for l in prange(n):
-            for o in prange(d):
-                for k in range(c):
-                    for j in range(padding2,jlim,stride):
-                        for i in range(padding2,ilim,stride):
-                            for n in range(kernel_size):
-                                for m in range(kernel_size):
-                                    output[l,o,(j-padding2)//stride,(i-padding2)//stride] += (padded_prev_grad[l,k,j-(padding2)+n,i-(padding2)+m]*prev_grad[k,o,n,m])
-                                      
-        return output
-
-        return None
-
+        for l in range(n):
+            padded_data_slice = padded_data[l]
+            padded_output_slice = padded_output[l]
+            for v in range(c):
+                for j in range(hout):
+                    for i in range(wout):
+                        h1 = j*stride
+                        h2 = j*stride+k
+                        w1 = i*stride
+                        w2 = i*stride+k
+                        padded_data_slice_smaller = padded_data_slice[:,h1:h2,w1:w2]
+                        padded_output_slice[:,h1:h2,w1:w1] += weight[:,v,:,:]*prev_grad[l,v,j,i]
+                        #output[l,:,h1:h2,w1:w2] += (prev_grad[l,o,j,i]*weight[:,o,:,:])
+                        bias_grad[v] += prev_grad[l,v,j,i]
+                        #weight_grad[:,o,:,:] += padded_data_slice_smaller*prev_grad[l,o,j,i]
+            if(padding == 0):
+                output[l,:,:,:] = padded_output_slice[:,:,:]
+            else:
+                output[l,:,:,:] = padded_output_slice[:,padding:-padding,padding:-padding]
+        return(output)
+                 
     def backward(self, previous_partial_gradient):
-        padding2 = self.padding
-        padded_grad = np.pad(previous_partial_gradient,((0,0),(0,0),(padding2,padding2),(padding2,padding2)),'constant')
-        output_array = np.zeros(np.shape(self.data), dtype = np.float32)
-        weight_grad = np.zeros((np.shape(self.weight.data)),dtype = np.float32)
-        back = backward_numba(self.padded_data, np.transpose(self.padded_data,(1,0,2,3)), output_array, previous_partial_gradient, padded_grad, self.weight.data,np.transpose(self.weight.data,(1,0,3,2)), self.weight.grad, self.padding, self.stride, self.kernel_size, self.padded_data.shape, previous_partial_gradient.shape, padding2)
-        return None
+        padded_output_array = np.zeros(self.padded_data.shape,dtype=np.float32)
+
+        output_array = np.zeros(self.data.shape,dtype=np.float32)
+
+           
+        back = self.backward_numba(self.padded_data, previous_partial_gradient, self.weight.data, self.weight.grad, self.bias.grad,output_array,padded_output_array, self.padding, self.stride)
+
+        
+        return back
 
     def selfstr(self):
         return "Kernel: (%s, %s) In Channels %s Out Channels %s Stride %s" % (
