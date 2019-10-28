dd8ce2becf60ba608a4a4139810d3192d25b956d
anushgandra <anushgandra@yahoo.com>
2019-10-28 10:13:12 -0700

    10/28/2019

--- nn/layers/max_pool_layer.py
+++ nn/layers/max_pool_layer.py
@@ -12,18 +12,41 @@ class MaxPoolLayer(Layer):
         self.kernel_size = kernel_size
         self.padding = (kernel_size - 1) // 2
         self.stride = stride
+        self.data = None
+        self.padded_data = None
 
     @staticmethod
     @njit(parallel=True, cache=True)
     def forward_numba(data):
-        # data is N x C x H x W
-        # TODO
-        return None
+        s = data.shape
+        n = s[0]
+        h = s[2]
+        w = s[3]
+        d = s[1]
+
+        jlim = (h-kernel_size) + 1 
+        ilim = (w-kernel_size) + 1 
+
+                  
+        for l in range(n):
+            for o in range(d):
+                for j in range(0,jlim,stride):
+                    for i in range(0,ilim,stride):
+                        
+                        out[l,o,j//stride,i//stride] = np.max(data[l,:,j:j+(kernel_size),i:i+(kernel_size)])
+                                                                        
+        return out
 
     def forward(self, data):
-        # TODO
-        return None
+        padded = np.pad(data,((0,0),(0,0),(self.padding,self.padding),(self.padding,self.padding)),'constant')
+        output_array = np.zeros((data.shape[0],self.weight.data.shape[1],(padded.shape[2]-self.kernel_size)//self.stride + 1,(padded.shape[3]-self.kernel_size)//self.stride + 1),dtype=np.float32)
 
+        output = self.forward_numba(padded, self.stride, self.kernel_size,output_array)
+        
+        self.data = data
+        self.padded_data = padded
+        return output
+        
     @staticmethod
     @njit(cache=True, parallel=True)
     def backward_numba(previous_grad, data):
