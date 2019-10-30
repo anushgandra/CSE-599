2bc525a2bebccec09a8145541d07068c07791ccf
anushgandra <anushgandra@yahoo.com>
2019-10-30 14:40:18 -0700

    Finished

--- nn/layers/add_layer.py
+++ nn/layers/add_layer.py
@@ -1,26 +1,24 @@
 from collections.abc import Iterable
 from typing import Tuple
 
-import numpy as np
-
 from .layer import Layer
-
+import numpy as np
 
 class AddLayer(Layer):
     def __init__(self, parents):
         super(AddLayer, self).__init__(parents)
-        self.data = None
+        self.data_shape = None
 
     def forward(self, inputs: Iterable):
         # TODO: Add all the items in inputs. Hint, python's sum() function may be of use.
-        output = np.sum(inputs,axis=0)
-        self.data = inputs
+        output = sum(inputs)
+        self.data_shape = np.shape(inputs)
         return output
 
     def backward(self, previous_partial_gradient) -> Tuple[np.ndarray, ...]:
         # TODO: You should return as many gradients as there were inputs.
         #   So for adding two tensors, you should return two gradient tensors corresponding to the
         #   order they were in the input.
-        output = (previous_partial_gradient,)*self.data.shape[0]
+        output = (previous_partial_gradient,)*self.data_shape[0]
                 
         return output
--- nn/layers/block_layers/res_net_block.py
+++ nn/layers/block_layers/res_net_block.py
@@ -5,8 +5,8 @@ class ResNetBlock(LayerUsingLayer):
     def __init__(self, conv_params, parent=None):
         super(ResNetBlock, self).__init__(parent)
         self.conv_layers = SequentialLayer([ConvLayer(*conv_params), ReLULayer(), ConvLayer(*conv_params)], self.parent)
-        self.add_layer = AddLayer(self.parent)
-        self.relu2 = ReLULayer(self.parent)
+        self.add_layer = AddLayer((self.conv_layers.final_layer,self.parent))
+        self.relu2 = ReLULayer(self.add_layer)
         assert not any([parent is None for parent in self.conv_layers.parents])
         assert not any([parent is None for parent in self.add_layer.parents])
         assert not any([parent is None for parent in self.relu2.parents])
